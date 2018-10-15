169243f0ac13695a2353a3cd1a7bd9845a76aff3
Nathaniel Stevens <nathaniel.scott.stevens@gmail.com>
2018-10-14 22:06:35 -0400

    get-neighbors now returns only points in bounds

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -2,10 +2,15 @@
   (:gen-class))
 
 (defn get-neighbors [grid point]
-  (->> (doall (for [x (range -1 2) y (range -1 2)] [x y]))
-       (filter #(not= % [0 0]))
-       (map #(map + % point))
-       (set)))
+  (let [max [(count grid) (count (grid 0))]]
+    (->> (doall (for [x (range -1 2) y (range -1 2)] [x y]))
+         (filter #(not= % [0 0]))
+         (map #(map + % point))
+         (filter #(< (first %) (first max)))
+         (filter #(< (last %) (last max)))
+         (filter #(>= (first %) 0))
+         (filter #(>= (last %) 0))
+         (set))))
 
 
 
--- test/game_of_life/core_test.clj
+++ test/game_of_life/core_test.clj
@@ -7,3 +7,12 @@
 (deftest get-neighbors-returns-neighbors
   (is (= (get-neighbors default-grid [1 1]) 
          #{[0 0] [1 0] [2 0] [0 1] [2 1] [0 2] [1 2] [2 2]})))
+
+(deftest get-neighbors-does-not-return-max-out-of-bounds
+  (is (= (get-neighbors default-grid [2 2]) 
+         #{[2 1] [1 1] [1 2]})))
+
+(deftest get-neighbors-does-not-return-min-out-of-bounds
+  (is (= (get-neighbors default-grid [0 0]) 
+         #{[1 0] [1 1] [0 1]})))
+
